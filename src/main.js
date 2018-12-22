59bdd16bcf81b0c9f0599290d987b8b9463cbc1d
macnie <mac@lenmy.com>
2018-12-22 14:44:41 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import router from './router'
 import ElementUI from 'element-ui';//引用ElementUI框架
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
-require('./mock/mock')
 Vue.use(ElementUI);
 Vue.prototype.$http =axios;
 
